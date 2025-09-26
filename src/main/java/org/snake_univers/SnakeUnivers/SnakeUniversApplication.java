5d3d0b66e36e03a9da762a3a539c22f1dccafd4e
moritzrose <moritz_rose@yahoo.de>
2025-09-26 16:49:29 +0200

    initial commit

---
+++ src/main/java/org/snake_univers/SnakeUnivers/SnakeUniversApplication.java
@@ -0,0 +1,13 @@
+package org.snake_univers.SnakeUnivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeUniversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeUniversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/snake_univers/SnakeUnivers/SnakeUniversApplicationTests.java
@@ -0,0 +1,13 @@
+package org.snake_univers.SnakeUnivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeUniversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
